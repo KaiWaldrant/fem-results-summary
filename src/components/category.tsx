f0a8565c552609771b570d401100f21ede9d3e64
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-09 14:12:49 +0100

    Add category comp

---
+++ src/components/category.tsx
@@ -0,0 +1,27 @@
+interface CategoryProps {
+  icon: string;
+  category: string;
+  score: number;
+}
+
+const catColor = {
+  Reaction: "color-red-",
+  Memory: "color-yellow-",
+  Verbal: "color-green-",
+  Visual: "color-blue-",
+};
+
+export default function Category(data: CategoryProps) {
+  return (
+    <div
+      key={data.category}
+      className={`category ${catColor[data.category] + "50"}`}
+    >
+      <img src={data.icon} alt={data.category} />
+      <h3>{data.category}</h3>
+      <p>
+        <span>{data.score}</span> / 100
+      </p>
+    </div>
+  );
+}
