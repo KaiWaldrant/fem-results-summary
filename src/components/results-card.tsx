c58197291133169d51e510266e7d0b70aa50fbee
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-10 15:11:08 +0100

    calculate average score

--- src/components/results-card.tsx
+++ src/components/results-card.tsx
@@ -2,17 +2,14 @@ import ResultBadge from "./result-badge";
 import type { JsonProps } from "../ types / types";
 
 export default function ResultsCard(data: JsonProps[]) {
-  let result;
-
-  data.map((item) => {
-    result = item.score;
-  });
+  const scores = data.map((item) => item.score);
+  const averageScore = scores.reduce((a, b) => a + b, 0) / scores.length;
 
   return (
     <div className="results-card">
       <div className="results-card__content">
         <h2 className="results-card__title">Your Results</h2>
-        <Resultbadge result={result} />
+        <Resultbadge result={Math.round(averageScore)} />
         <p className="results-card__score">Great</p>
         <p className="results-card__description">
           You scored higher than 65% of the people who have taken these tests.
