7f16c1f9cf2b84ce7b09b4edc6f04d77da3ef848
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-12 12:15:56 +0100

    Add main styling summary section

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -5,22 +5,30 @@ export type CategoryProps = {
 };
 
 const catColor = {
-  Reaction: "color-red-",
-  Memory: "color-yellow-",
-  Verbal: "color-green-",
-  Visual: "color-blue-",
+  Reaction: "red",
+  Memory: "yellow",
+  Verbal: "green",
+  Visual: "blue",
 };
+
+function getIconPath(jsonPath: string) {
+  // Remove the leading "./" and you have the public path
+  return jsonPath.replace("./", "/");
+}
 
 export default function Category({ data }: CategoryProps) {
   return (
-    <div
-      key={data.category}
-      className={`category ${catColor[data.category] + "50"}`}
-    >
-      <img src={data.icon} alt={data.category} />
-      <h3>{data.category}</h3>
-      <p>
-        <span>{data.score}</span> / 100
+    <div key={data.category} className={`category ${catColor[data.category]}`}>
+      <div className="category__content">
+        <img
+          className="category__icon"
+          src={getIconPath(data.icon)}
+          alt={data.category}
+        />
+        <h3 className="category__title">{data.category}</h3>
+      </div>
+      <p className="category__score">
+        <span className="category__score-value">{data.score}</span> / 100
       </p>
     </div>
   );
--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -10,12 +10,12 @@ export default function Summary({ data }: SummaryProps) {
   return (
     <div className="summary">
       <h2 className="summary__header">Summary</h2>
-      <div className="summary__content">
+      <div className="summary__categories">
         {data.map((cat) => (
           <Category data={cat} />
         ))}
-        <Button>Continue</Button>
       </div>
+      <Button className="button">Continue</Button>
     </div>
   );
 }
